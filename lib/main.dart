ab8bfcd81cf6ea3c396ddc2838d720826fb80ce6
JoseManuelLopez303 <hermanoslopez539@gmail.com>
2021-11-29 00:25:19 -0700

    1 xd

---
+++ lib/main.dart
@@ -0,0 +1,58 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:http/http.dart' as http;
+import 'package:my_app/login.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Examen",
+      debugShowCheckedModeBanner: false,
+      home: MainPage(),
+      theme: ThemeData(
+        accentColor: Colors.white70
+      ),
+    );
+  }
+}
+
+class MainPage extends StatefulWidget {
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+
+  SharedPreferences sharedPreferences;
+
+  @override
+  void initState() {
+    super.initState();
+    checkLoginStatus();
+  }
+
+  checkLoginStatus() async {
+    sharedPreferences = await SharedPreferences.getInstance();
+    if(sharedPreferences.getString("token") == null) {
+      Navigator.of(context).pushAndRemoveUntil(MaterialPageRoute(builder: (BuildContext context) => LoginPage()), (Route<dynamic> route) => false);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Comunidad lolsito xd", style: TextStyle(color: Colors.white)),
+
+      ),
+      body: Center(child: Text("Que tranzaaaaaaaa")),
+      drawer: Drawer(),
+    );
+  }
+}
